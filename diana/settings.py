9809943dd5eff9b46b4d29fa4fcc8fc34e234c3a
benn1907 <36059686+benn1988@users.noreply.github.com>
2019-08-09 21:22:29 -0700

    testing deploy

--- diana/settings.py
+++ diana/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -132,3 +132,8 @@ LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
+
+
+# Configure Django App for Heroku.
+
+django_heroku.settings(locals())
